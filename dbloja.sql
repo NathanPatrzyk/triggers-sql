a4cfac6270d089db8df253f5ea8b9b2eabfc172f
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-08-28 17:45:52 -0300

    Teste e correção dos scripts de criação das tabelas

--- dbloja.sql
+++ dbloja.sql
@@ -1,59 +1,59 @@
 DROP DATABASE IF EXISTS dbloja;
+
 CREATE DATABASE dbloja;
+
 USE dbloja;
 
-CREATE TABLE tbclientes(
+CREATE TABLE tbcliente (
     cpf_cliente INT(11) NOT NULL,
-    nome_cliente VARCHAR(50),
+    nome_cliente VARCHAR(100),
     telefone_cliente INT(14),
-    endereco_cliente VARCHAR (150),
-    CONSTRAINT pk_cliente PRIMARY KEY(cpf_cliente)
+    endereco_cliente VARCHAR(200),
+    CONSTRAINT pk_tbcliente PRIMARY KEY (cpf_cliente)
 ) ENGINE = INNODB;
 
-CREATE TABLE tbfornecedores(
-    cnpj_fornecedor INT(20) NOT NULL,
-    nome_empresa_fornecedor VARCHAR(50),
-    nome_representante_fornecedor VARCHAR(50),
-    telefone_cliente INT(14),
-    endereco_cliente VARCHAR (150),
-    CONSTRAINT pk_fornecedor PRIMARY KEY(cnpj_fornecedor)
+CREATE TABLE tbfornecedor (
+    cnpj_fornecedor INT(14) NOT NULL,
+    nome_empresa_fornecedor VARCHAR(100),
+    nome_representante_fornecedor VARCHAR(100),
+    telefone_fornecedor INT(14),
+    endereco_fornecedor VARCHAR(200),
+    CONSTRAINT pk_tbfornecedor PRIMARY KEY (cnpj_fornecedor)
 ) ENGINE = INNODB;
 
-CREATE TABLE tbprodutos(
-    codigo_produto INT (11) NOT NULL,
-    nome_produto VARCHAR(25),
-    preco_unitario DOUBLE,
-    cnpj_fornecedor INT(20),
-    CONSTRAINT pk_produto PRIMARY KEY(codigo_produto),
-    CONSTRAINT fk_produto_fornecedor FOREIGN KEY (cnpj_fornecedor) REFERENCES tbfornecedor(cnpj_fornecedor) 
-    ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE tbproduto (
+    codigo_produto INT(11) NOT NULL,
+    nome_produto VARCHAR(100),
+    preco_produto DECIMAL(10, 2),
+    cnpj_fornecedor INT(14),
+    CONSTRAINT pk_tbproduto PRIMARY KEY (codigo_produto),
+    CONSTRAINT fk_tbproduto_tbfornecedor FOREIGN KEY (cnpj_fornecedor) REFERENCES tbfornecedor (cnpj_fornecedor) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
-CREATE TABLE tbpagamentos(
-    codigo_pagamento INT (11) NOT NULL,
-    forma_pagamento VARCHAR(50),
-    valor_total DOUBLE,
-    valor_pago DOUBLE,
-    codigo_pedido INT (11),
-    CONSTRAINT pk_pagamento PRIMARY KEY(ccodigo_pagamento),
-    CONSTRAINT fk_pagamento_pedido FOREIGN KEY (codigo_pagamento) REFERENCES tbpedido(codigo_pedido) 
-    ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE tbpagamento (
+    codigo_pagamento INT(11) NOT NULL,
+    forma_pagamento VARCHAR(100),
+    status_pagamento VARCHAR(100),
+    CONSTRAINT pk_tbpagamento PRIMARY KEY (codigo_pagamento)
 ) ENGINE = INNODB;
 
-CREATE TABLE tbpedidos(
-    codigo_pedido INT(11)
+CREATE TABLE tbpedido (
+    codigo_pedido INT(11) NOT NULL,
+    valor_total DECIMAL(10, 2),
     data_hora_pedido DATETIME,
-    cpf_cliente INT(11)
-    CONSTRAINT pk_pedidos PRIMARY KEY(codigo_pedido),
-    CONSTRAINT fk_pedido_cliente FOREIGN KEY (cpf_cliente) REFERENCES tbclientes(cpf_cliente) 
-    ON DELETE CASCADE ON UPDATE CASCADE
+    cpf_cliente INT(11),
+    codigo_pagamento INT(11),
+    CONSTRAINT pk_tbpedido PRIMARY KEY (codigo_pedido),
+    CONSTRAINT fk_tbpedido_tbcliente FOREIGN KEY (cpf_cliente) REFERENCES tbcliente (cpf_cliente),
+    CONSTRAINT fk_tbpedido_tbpagamento FOREIGN KEY (codigo_pagamento) REFERENCES tbpagamento (codigo_pagamento) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
 
-CREATE TABlE tbitens(
-    codigo_item INT (11) NOT NULL,
+CREATE TABlE tbitem (
+    codigo_item INT(11) NOT NULL,
     quantidade_item INT(3) NOT NULL,
-    codigo_produto INT (11),
-    CONSTRAINT pk_item PRIMARY KEY(codigo_item),
-    CONSTRAINT fk_item_produto FOREIGN KEY (codigo_produto) REFERENCES tbprodutos(codigo_produto) 
-    ON DELETE CASCADE ON UPDATE CASCADE
+    codigo_pedido INT(11),
+    codigo_produto INT(11),
+    CONSTRAINT pk_tbitem PRIMARY KEY (codigo_item),
+    CONSTRAINT fk_tbitem_tbpedido FOREIGN KEY (codigo_pedido) REFERENCES tbpedido (codigo_pedido),
+    CONSTRAINT fk_tbitem_tbproduto FOREIGN KEY (codigo_produto) REFERENCES tbproduto (codigo_produto) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = INNODB;
